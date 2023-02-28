ce2185615df66e94b835f21c74044f7adf004526
Clement Levallois <1244100+seinecle@users.noreply.github.com>
2023-02-28 21:47:52 +0100

    tried to solve the viewhandler whatever logging issue

--- src/main/java/net/clementlevallois/nocodeapp/web/front/NoCacheFilter.java
+++ src/main/java/net/clementlevallois/nocodeapp/web/front/NoCacheFilter.java
@@ -13,6 +13,8 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,17 +24,21 @@ import jakarta.servlet.http.HttpServletResponse;
 public class NoCacheFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-        if (!req.getRequestURI().startsWith(req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
-            res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-            res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-            res.setDateHeader("Expires", 0); // Proxies.
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain){
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
+            
+            if (!req.getRequestURI().startsWith(req.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
+                res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+                res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+                res.setDateHeader("Expires", 0); // Proxies.
+            }
+            
+            chain.doFilter(request, response);
+        } catch (IOException | ServletException ex) {
+            Logger.getLogger(NoCacheFilter.class.getName()).log(Level.SEVERE, null, "*** error in filter ***");
         }
-
-        chain.doFilter(request, response);
     }
 
     // ...
