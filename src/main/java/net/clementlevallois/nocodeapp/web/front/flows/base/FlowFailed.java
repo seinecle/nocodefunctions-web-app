f71cae0f81283d793c5b18b59512e78f4fed06f5
Your Name <you@example.com>
2025-09-08 14:42:15 +0200

    chore: supprimer la ligne vide inutile dans FlowFailed.java

--- src/main/java/net/clementlevallois/nocodeapp/web/front/flows/base/FlowFailed.java
+++ src/main/java/net/clementlevallois/nocodeapp/web/front/flows/base/FlowFailed.java
@@ -2,5 +2,4 @@ package net.clementlevallois.nocodeapp.web.front.flows.base;
 
 public record FlowFailed(String jobId, FlowState lastKnownState, String userMessage, String logMessage) implements FlowState {
     
-    
 }
